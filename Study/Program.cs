7185e8875292e30376b12195445decdd5d4454f0
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-08-13 17:44:23 +0500

    Добавьте файлы проекта.

---
+++ Study/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CSharpLearning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Как вас зовут? ");
+            string name = Console.ReadLine();
+            Console.Write("Какой ваш знак зодиака? ");
+            string zodiac = Console.ReadLine();
+            Console.Write("Сколько вам лет? ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Где вы работаете? ");
+            string workPlace = Console.ReadLine();
+
+            Console.WriteLine($"Вас зовут {name}, вам {age} лет, вы {zodiac} и работаете {workPlace}.");
+        }
+    }
+}
